097f1a1fab5772ae8dc363963e85ea2d3b8a63c7
mattpaul <mattpaul@gmail.com>
2018-07-25 11:21:17 -0700

    implement metric summation over most recent hour via array reduce

--- index.js
+++ index.js
@@ -8,35 +8,46 @@ global.metrics = {}
 
 // return summation of metric values for key
 app.get('/metric/:key/sum', function (req, res) {
-  var value = 0
-  key = req.params.key
+  var key = req.params.key
+  var sum = 0
 
   if (global.metrics[key] !== undefined) {
-    value = global.metrics[key]
+    var initialValue = 0
+    sum = global.metrics[key].reduce(function (accumulator, currentValue) {
+      var now = ((new Date).getTime()/1000).toFixed()
+      if (now - currentValue.time < 3600) {
+        accumulator += currentValue.value
+      }
+      return accumulator
+    }, initialValue)
+  } else {
+    sum = 0
   }
 
   response = {
-    "value": value
+    "value": sum
   }
 
-  console.log("SUM " + key + ": " + value)
+  console.log("SUM " + key + ": " + sum)
   res.send(response)
 })
 
 // log new metric value for key
 app.post('/metric/:key', jsonParser, function (req, res) {
   if (req.body.value) {
+    var key = req.params.key
+    var time = ((new Date).getTime()/1000).toFixed()
     var value = req.body.value
-    key = req.params.key
 
     if (global.metrics[key] === undefined) {
-      global.metrics[key] = value
-    } else {
-      global.metrics[key] += value
+      global.metrics[key] = []
     }
+
+    global.metrics[key].push({ time: time, value: value })
   }
 
   console.log("LOG " + key + ": " + value)
+  console.log(global.metrics[key])
   res.status(200)
   res.send()
 })
